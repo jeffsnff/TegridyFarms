b2bb78053796a9f2b0eaf266a6d425f909e87f34
Jeffrey Seneff <jeffsnff@gmail.com>
2019-10-03 17:54:15 -0600

    put my twitter page link and git hub link into the foter. I also added images for them to be links.

--- myy-app/src/footer/Footer.js
+++ myy-app/src/footer/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Footer.css';
+import twitter from '../photos/twitter.png'
+import github from '../photos/github.png'
 /* 
 1. Add twitter icon to the Tegerdy Farms twitter pageBreakAfter
 const towly = (arg) => console.log(`
@@ -12,9 +14,9 @@ const Footer = () => {
     
     return (
         <div className="footer">
-            <a href=".">Something</a>
+            <a href="https://github.com/jeffsnff"><img src={github} alt="my github profile link"/></a>
             <p>1402 Tegridy Farms,<br/> Colorado Farm, CO</p>
-            <a href="https://twitter.com/tegridy_farms?lang=en" rel="noopener noreferrer" target="_blank">Twitter</a>
+            <a href="https://twitter.com/tegridy_farms?lang=en" rel="noopener noreferrer" target="_blank"><img src={twitter} alt="twitter link"/></a>
         </div>
     )
 }
