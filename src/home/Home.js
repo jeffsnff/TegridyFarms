42f3c2d575ae04d20a1ed73ad97835cb9fc979dd
Jeffrey Seneff <jeffsnff@gmail.com>
2019-11-27 14:00:56 -0700

    Finished redesign of homepage. Had to tweak NavBar to make it better.

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,34 +1,38 @@
 import React from 'react';
 import './Home.css'
-import Frame from '../photos/Frame.png'
 
 import cannabis from '../photos/cannabis.jpg'
-
+import farm from '../photos/TegFarmsFarm2.jpg'
 function Home(){
 
     return (
     <div className="main-containter">
 
         <div className="home">
+            <div className="overlay">
+                <img className="farm" src={farm} />
+            </div>
+            
             <h1 className="homeText">Tegridy Farms Strain Finder</h1>
             <div className="about">
                     We want to help you find the perfect strain to fit your needs. At Tegridy Farms, we strive to bring Tegridy into every step of our pricess in order to give you the best experiencd possible.
             </div>
-            <div className="button">
+            <button className="button">
                 View All Strains
-            </div>
+            </button>
+
         </div>
 
         <div className="second-container">
             <div className="haha">
-                <img src={Frame} alt="this" />
-                 <div className="stuff">
+                 <img src={cannabis} alt="this" />
+                <div className="stuff">
                     <h3>Why Tegridy?</h3>
                     <p>
                     We want to help you find the perfect strain to fit your needs. At Tegridy Farms, we strive to bring Tegridy into every step of our pricess in order to give you the best experiencd possible.
                     </p>
                     
-                </div>
+                </div> 
                 <button className="rrrhhh" onClick={() => console.log("hello")}>
                     LEARN MORE
                 </button>
