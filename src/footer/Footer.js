1ae850c193b0e91422d6304e65cea4605e31ee09
Jeffrey Seneff <jeffsnff@gmail.com>
2019-11-26 18:02:41 -0700

    redesigned footer based on specs. Also fixed up the navbar a little

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -2,21 +2,26 @@ import React from 'react';
 import './Footer.css';
 import twitter from '../photos/twitter.png'
 import github from '../photos/github.png'
-/* 
-1. Add twitter icon to the Tegerdy Farms twitter pageBreakAfter
-const towly = (arg) => console.log(`
-Towly: ${arg}
-"That's some good shit!"
-`)towly(39457934895)
-*/
 
 const Footer = () => {
-    
     return (
         <div className="footer">
-            <a href="https://github.com/jeffsnff" target="_blank" rel="noopener noreferrer"><img src={github} alt="my github profile link"/></a>
-            <p>1402 Tegridy Farms,<br/> Colorado Farm, CO</p>
-            <a href="https://twitter.com/tegridy_farms?lang=en" rel="noopener noreferrer" target="_blank"><img src={twitter} alt="twitter link"/></a>
+
+            <div className="information">
+                <div>
+                    <h6>Tegridy Farms</h6>
+                    <p>1402 Tegridy Ln</p>
+                    <p>Colorado Farms, CO</p>
+                    <p className="name">Development By: <span>Jeffrey Seneff</span></p>
+                    <p className="name">Design By: <span>Cody Porter</span></p>
+                </div>
+            </div>
+
+            <div className="socialMedia">
+                <a href="https://github.com/jeffsnff" target="_blank" rel="noopener noreferrer"><img className="socialLinks"src={github} alt="my github profile link"/></a>
+                <a href="https://twitter.com/tegridy_farms?lang=en" rel="noopener noreferrer" target="_blank"><img className="socialLinks"src={twitter} alt="twitter link"/></a>
+            </div>
+
         </div>
     )
 }
