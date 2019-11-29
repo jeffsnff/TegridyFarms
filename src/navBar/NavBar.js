bda19af4f44921b398f7245fa82bf14aabdf402e
Jeffrey Seneff <jeffsnff@gmail.com>
2019-11-29 13:25:02 -0700

    Made module for different strains search

--- src/navBar/NavBar.js
+++ src/navBar/NavBar.js
@@ -1,33 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 import logo from '../photos/swallow.jpg'
 
-function NavBar(){
-    return(
-        <div className="headerContainer">
-            <ul className="navigation-menu">
-                <div className='logo'>
-                    <Link to="/"><img className='icon' src={logo} alt="logo" /></Link>
-                </div>
-                
+class NavBar extends Component {
+    constructor(){
+        super()
+        this.state = {
+            toggle: false,
+            color: 'white'
+        }
+    }
 
-                <div className="navigation">
-                    <div className="strainsDiv">
-                        <Link className='strainsLink' to="/ProductList">Strains</Link>
+    toggler = () => {
+        this.setState({toggle: !this.state.toggle})
+        
+    }
+    
+    render(){
+        return(
+            <div className="headerContainer">
+                <ul className="navigation-menu">
+                    <div className='logo'>
+                        <Link to="/"><img className='icon' src={logo} alt="logo" /></Link>
                     </div>
-                    <div className="aboutDiv">
-                        <Link className='aboutLink' to="/About">About</Link>
+ 
+                    <div className="navigation">
+                        <div className="strainsDiv">
+                            {/* <Link className='strainsLink' to="/ProductList">Strains</Link> */}
+                            {
+                                this.state.toggle ?
+                                    <div 
+                                        className="strainsLink" 
+                                        onClick={() => this.toggler()}
+                                        style={{color: this.state.color}}
+                                        >
+                                        Strains</div>
+                                :
+                                    <div className="strainsLink" onClick={() => this.toggler()}>Strains</div>
+                            }
+                            <div className="strainsLink" onClick={() => this.toggler()}>Strains</div>
+                            {
+                                this.state.toggle ?
+                                    <div className="strainModule">
+                                        <Link className='allLink' to='/ProductList'>All Strains</Link>
+                                        <Link className='indicaLink' to='/ProductList'>Indica</Link>
+                                        <Link className='sativaLink' to='/ProductList'>Sativa</Link>
+                                        <Link className='hybridLink' to='/ProductList'>Hybrid</Link>
+                                    </div>
+                                    :
+                                    null
+                            }
+                        </div>
+
+                        <div className="aboutDiv">
+                            <Link className='aboutLink' to="/About">About</Link>
+                        </div>
+
+                        <div className="contactDiv">
+                            <Link className='contactLink' to="/Contact">Contact</Link>
+                        </div>
                     </div>
-                    <div className="contactDiv">
-                        <Link className='contactLink' to="/Contact">Contact</Link>
-                    </div>
-                </div>
-                
-            </ul>  
-        </div>
-    )
+                    
+                </ul>  
+            </div>
+        )
+    }
+    
 }
 
 export default NavBar;
