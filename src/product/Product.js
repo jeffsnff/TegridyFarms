2d435b14ff0aa60b9779188daec38ada992b9c33
Jeffrey Seneff <jeffsnff@gmail.com>
2019-11-30 15:06:40 -0700

    displayed name of race on product card. Messed with the css to display search and filter cards.

--- src/product/Product.js
+++ src/product/Product.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 import './Product.css';
 
 const Product = props => {
-  
+        
         return (
             <div>
                 <Link to={{pathname:`/ProductDetail/${props.name}`, state: {id:props.id, array:props.array, img:props.img}}}>
                 <div className="product">
                     <h2>{props.name}</h2>
-                    <h4>{props.id}</h4>
+                    <h4>{props.race}</h4>
                     <img src={props.img} alt={props.img} />
                 </div> 
                 </Link>
