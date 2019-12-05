2938885f2d4c8ae449750d997ca80732a54cf7bb
Jeffrey Seneff <jeffsnff@gmail.com>
2019-12-05 15:16:16 -0700

    Redesigned the product cards on detail list

--- src/product/Product.js
+++ src/product/Product.js
@@ -8,9 +8,11 @@ const Product = props => {
             <div>
                 <Link to={{pathname:`/ProductDetail/${props.name}`, state: {id:props.id, array:props.array, img:props.img}}}>
                 <div className="product">
-                    <h2>{props.name}</h2>
-                    <h4>{props.race}</h4>
                     <img src={props.img} alt={props.img} />
+                    <h2>{props.name}</h2>
+                    <div>
+                        <h4>{props.race}</h4>
+                    </div>
                 </div> 
                 </Link>
             </div>
