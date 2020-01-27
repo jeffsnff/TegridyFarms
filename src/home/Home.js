f2cb6b186bcaeb421a9cbe745bedabdf87fca044
Jeffrey Seneff <jeffsnff@gmail.com>
2020-01-26 16:58:46 -0800

    Home now scolls to top on load.

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import './Home.css'
 
@@ -6,54 +6,63 @@ import Button from '../button/Button.js'
 
 import cannabis from '../photos/TegFarmsTegridy.jpg'
 import farm from '../photos/TegFarmsFarm2.jpg'
-function Home(){
-    return (
-    <div className="main-containter">
+class Home extends Component{
+    constructor(){
+        super()
+    }
 
-        <div className="home">
-            <div className="overlay">
-                <img className="farm" src={farm} alt="family fdarm" />
-            </div>
-            
-            <h1 className="homeText">Tegridy Farms Strain Finder</h1>
-            <div className="about">
-                    We want to help you find the perfect strain to fit your needs. At Tegridy Farms, we strive to bring Tegridy into every step of our pricess in order to give you the best experiencd possible.
-            </div>
-            {/* <Link className="button" to='/ProductList'>
-                View All Strains
-            </Link> */}
-            <Button 
-                text='View All Strains'
-                destination='/ProductList'
-                name='button'
-            />
+    componentDidMount(){
+        window.scrollTo(0,0)
+    }
+    render(){
+        return (
+        <div className="main-containter">
 
-        </div>
-
-        <div className="second-container">
-            <div className="haha">
-                 <img src={cannabis} alt="this" />
-                <div className="stuff">
-                    <h3>Why Tegridy?</h3>
-                    <p>
-                    We want to help you find the perfect strain to fit your needs. At Tegridy Farms, we strive to bring Tegridy into every step of our pricess in order to give you the best experiencd possible.
-                    </p>
-                    
-                </div> 
-                {/* <Link className="rrrhhh" to="/About">
-                    LEARN MORE
+            <div className="home">
+                <div className="overlay">
+                    <img className="farm" src={farm} alt="family fdarm" />
+                </div>
+                
+                <h1 className="homeText">Tegridy Farms Strain Finder</h1>
+                <div className="about">
+                        We want to help you find the perfect strain to fit your needs. At Tegridy Farms, we strive to bring Tegridy into every step of our pricess in order to give you the best experiencd possible.
+                </div>
+                {/* <Link className="button" to='/ProductList'>
+                    View All Strains
                 </Link> */}
                 <Button 
-                    name='rrrhhh'
-                    destination='/About'
-                    text="LEARN MORE"
+                    text='View All Strains'
+                    destination='/ProductList'
+                    name='button'
                 />
-            </div>
-            
-        </div>
 
-    </div>
-    )
+            </div>
+
+            <div className="second-container">
+                <div className="haha">
+                    <img src={cannabis} alt="this" />
+                    <div className="stuff">
+                        <h3>Why Tegridy?</h3>
+                        <p>
+                        We want to help you find the perfect strain to fit your needs. At Tegridy Farms, we strive to bring Tegridy into every step of our pricess in order to give you the best experiencd possible.
+                        </p>
+                        
+                    </div> 
+                    {/* <Link className="rrrhhh" to="/About">
+                        LEARN MORE
+                    </Link> */}
+                    <Button 
+                        name='rrrhhh'
+                        destination='/About'
+                        text="LEARN MORE"
+                    />
+                </div>
+                
+            </div>
+
+        </div>
+        )
+    }
 }
 
 export default Home
